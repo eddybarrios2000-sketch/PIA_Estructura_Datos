0392115c3ffa0a9359fc8930a51ac99feff18feb
Eddy Barrios <eddybarrios2000@gmail.com>
2026-05-10 02:43:29 -0600

    update ordenamiento
    se implementó ordenamiento quicksort para los alumnos, tomar en cuenta la variable contador para que al momento de eliminar un alumno, el contador disminuya uno para que la busqueda binaria siga funcionando bien

--- PIA.cpp
+++ PIA.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 //Lista doblemente enlazada para almacenar los datos de los alumnos
@@ -8,10 +8,10 @@ using namespace std;
 struct Alumno
 {
     int matricula;
-    string nombre;
+    char nombre[100];
     int edad;
     float promedio_general;
-    string direccion;
+    char direccion[100];
     int telefono;
     Alumno* siguiente;
     Alumno* anterior;
@@ -24,91 +24,98 @@ void baja_alumno(Nodo_alumno& head, int contador, Nodo_alumno& stack_head);
 bool busqueda_binaria(Nodo_alumno& head, int matricula, int contador, Nodo_alumno& stack_head);
 void push(Nodo_alumno& stack_head, Nodo_alumno alumno);
 void pop(Nodo_alumno& stack_head);
+Nodo_alumno quick_sort(Nodo_alumno head);
+void insertar_en_lista(Nodo_alumno& headf, Nodo_alumno nuevo);
 
-
-
-
-main()
+int main()
 {
     int contador = 0;
-    int opcion; 
+    int opcion;
+    string ans;
+
     Nodo_alumno head = nullptr;
     Nodo_alumno stack_head = nullptr;
-
 
     do
     {
         cout << "-----INSTITUCION EDUCATIVA IMAK----- " << endl;
         cout << "1. Alta de alumnos" << endl;
-        cout << "2. Baja de alumnos" << endl;  
+        cout << "2. Baja de alumnos" << endl;
         cout << "3. Recuperar alumnos" << endl;
         cout << "4. Reportes " << endl;
         cout << "5. Control de inscripciones" << endl;
         cout << "6. Salir" << endl;
-        cout << "Ingrese una opcion: "; 
+        cout << "Ingrese una opcion: ";
         cin >> opcion;
 
         switch (opcion)
         {
             case 1:
                 cout << "--Alta de alumnos--" << endl;
-                string ans;
+
                 do
                 {
                     alta_alumno(head);
-                    //aqui iría el metodo de ordenamiento para ordenar a los alumnos cada que se ingrese uno nuevo
-                    contador ++;
+
+                    head = quick_sort(head);
+
+                    contador++;
+
                     cout << "Desea ingresar un nuevo alumno? (si/no): ";
                     cin >> ans;
-                    
-                }while (ans == "si");
-                
+
+                } while (ans == "si");
+
                 break;
 
             case 2:
                 baja_alumno(head, contador, stack_head);
                 break;
-            
+
             case 3:
-                
+
                 break;
 
-            case 4: 
-                
+            case 4:
+
                 break;
 
             case 5:
-                
+
                 break;
 
             case 6:
                 cout << "Saliendo del programa..." << endl;
                 break;
-            
+
             default:
                 break;
         }
-    }while (opcion != 6);
-   
-    
 
+    } while (opcion != 6);
 }
-
 
 void alta_alumno(Nodo_alumno& head)
 {
-
     Nodo_alumno nuevo_alumno = new Alumno;
+
     cout << "Ingrese la matricula del alumno: ";
     cin >> nuevo_alumno->matricula;
+
     cout << "Ingrese el nombre del alumno: ";
-    cin >> nuevo_alumno->nombre;
+    cin >> ws;
+    cin.get(nuevo_alumno->nombre, 100);
+
     cout << "Ingrese la edad del alumno: ";
     cin >> nuevo_alumno->edad;
+
     cout << "Ingrese el promedio general del alumno: ";
     cin >> nuevo_alumno->promedio_general;
+
     cout << "Ingrese la direccion del alumno: ";
-    cin >> nuevo_alumno->direccion;
+    cin >> ws;
+    cin.get(nuevo_alumno->direccion, 100);
+
     cout << "Ingrese el telefono del alumno: ";
     cin >> nuevo_alumno->telefono;
 
@@ -122,10 +129,12 @@ void alta_alumno(Nodo_alumno& head)
     else
     {
         Nodo_alumno temp = head;
+
         while (temp->siguiente != nullptr)
         {
             temp = temp->siguiente;
         }
+
         temp->siguiente = nuevo_alumno;
         nuevo_alumno->anterior = temp;
     }
@@ -135,24 +144,26 @@ void baja_alumno(Nodo_alumno& head, int contador, Nodo_alumno& stack_head)
 {
     int opcion;
     string ans;
+
     do
     {
-
-        cout << "Busqueda de alumno "<< endl;
+        cout << "Busqueda de alumno " << endl;
         cout << "1. Buscar por matricula" << endl;
         cout << "2. Buscar por nombre" << endl;
+
         cin >> opcion;
-        
 
         if (opcion == 1)
         {
             int matricula;
+
             cout << "Ingrese la matricula del alumno: ";
             cin >> matricula;
+
             if (busqueda_binaria(head, matricula, contador, stack_head))
             {
-
                 cout << "Alumno temporalmente eliminado con exito" << endl;
+                contador--;
             }
             else
             {
@@ -163,52 +174,86 @@ void baja_alumno(Nodo_alumno& head, int contador, Nodo_alumno& stack_head)
         else if (opcion == 2)
         {
             bool flag = true;
-            string nombre;
+
+            char nombre[100];
+
             cout << "Ingrese el nombre del alumno: ";
-            cin >> nombre;
+            cin >> ws;
+            cin.get(nombre, 100);
 
             Nodo_alumno temp = head;
+
             while (temp != nullptr)
             {
-                if (temp->nombre == nombre)
+                if (strcmp(temp->nombre, nombre) == 0)
                 {
                     push(stack_head, temp);
+
+                    if (temp == head)
+                    {
+                        head = temp->siguiente;
+
+                        if (head != nullptr)
+                        {
+                            head->anterior = nullptr;
+                        }
+                    }
+
+                    else if (temp->siguiente == nullptr)
+                    {
+                        temp->anterior->siguiente = nullptr;
+                    }
+
+                    else
+                    {
+                        temp->anterior->siguiente = temp->siguiente;
+                        temp->siguiente->anterior = temp->anterior;
+                    }
+
                     cout << "Alumno temporalmente eliminado con exito" << endl;
+                	contador--;
+
                     flag = false;
+
                     break;
                 }
+
                 temp = temp->siguiente;
             }
+
             if (flag)
             {
                 cout << "Alumno no encontrado" << endl;
             }
-
         }
-    
+
         cout << "Desea eliminar otro alumno? (si/no): ";
         cin >> ans;
 
     } while (ans == "si");
-    
 }
 
 bool busqueda_binaria(Nodo_alumno& head, int matricula, int contador, Nodo_alumno& stack_head)
 {
     int inicio = 0;
     int fin = contador - 1;
-
+	
+	if(head == nullptr) return false;
+	
     while (inicio <= fin)
     {
         int medio = (inicio + fin) / 2;
+
         Nodo_alumno temp = head;
+
         for (int i = 0; i < medio; i++)
         {
             temp = temp->siguiente;
         }
+
         if (temp->matricula == matricula)
         {
-           push(stack_head, temp);
+            push(stack_head, temp);
 
             if (temp == head)
             {
@@ -230,19 +275,23 @@ bool busqueda_binaria(Nodo_alumno& head, int matricula, int contador, Nodo_alumn
                 temp->anterior->siguiente = temp->siguiente;
                 temp->siguiente->anterior = temp->anterior;
             }
+
             delete temp;
 
             return true;
         }
+
         else if (temp->matricula < matricula)
         {
             inicio = medio + 1;
         }
+
         else
         {
             fin = medio - 1;
         }
     }
+
     return false;
 }
 
@@ -251,17 +300,23 @@ void push(Nodo_alumno& stack_head, Nodo_alumno alumno)
     if (stack_head == nullptr)
     {
         Nodo_alumno copia = new Alumno;
+
         *copia = *alumno;
+
         copia->anterior = nullptr;
         copia->siguiente = nullptr;
+
         stack_head = copia;
     }
     else
     {
         Nodo_alumno copia = new Alumno;
+
         *copia = *alumno;
+
         copia->anterior = nullptr;
         copia->siguiente = stack_head;
+
         stack_head = copia;
     }
 }
@@ -271,11 +326,101 @@ void pop(Nodo_alumno& stack_head)
     if (stack_head != nullptr)
     {
         Nodo_alumno temp = stack_head;
+
         stack_head = stack_head->siguiente;
+
         delete temp;
     }
-    else 
+    else
     {
         cout << "La pila esta vacia" << endl;
+    }
+}
+
+Nodo_alumno quick_sort(Nodo_alumno head)
+{
+    if (head == nullptr || head->siguiente == nullptr)
+    {
+        return head;
+    }
+
+    Nodo_alumno pivote = head;
+    Nodo_alumno actual = head->siguiente;
+
+    pivote->siguiente = nullptr;
+    pivote->anterior = nullptr;
+
+    Nodo_alumno menores = nullptr;
+    Nodo_alumno mayores = nullptr;
+
+    while (actual != nullptr)
+    {
+        Nodo_alumno siguiente = actual->siguiente;
+
+        actual->siguiente = nullptr;
+        actual->anterior = nullptr;
+
+        if (actual->matricula <= pivote->matricula)
+        {
+            insertar_en_lista(menores, actual);
+        }
+        else
+        {
+            insertar_en_lista(mayores, actual);
+        }
+
+        actual = siguiente;
+    }
+
+    menores = quick_sort(menores);
+    mayores = quick_sort(mayores);
+
+    Nodo_alumno resultado;
+
+    if (menores != nullptr)
+    {
+        resultado = menores;
+
+        Nodo_alumno temp = menores;
+
+        while (temp->siguiente != nullptr)
+        {
+            temp = temp->siguiente;
+        }
+
+        temp->siguiente = pivote;
+        pivote->anterior = temp;
+    }
+    else
+    {
+        resultado = pivote;
+    }
+
+    if (mayores != nullptr)
+    {
+        pivote->siguiente = mayores;
+        mayores->anterior = pivote;
+    }
+
+    return resultado;
+}
+
+void insertar_en_lista(Nodo_alumno& headf, Nodo_alumno nuevo)
+{
+    if (headf == nullptr)
+    {
+        headf = nuevo;
+    }
+    else
+    {
+        Nodo_alumno temp = headf;
+
+        while (temp->siguiente != nullptr)
+        {
+            temp = temp->siguiente;
+        }
+
+        temp->siguiente = nuevo;
+        nuevo->anterior = temp;
     }
 }
